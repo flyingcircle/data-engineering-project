42c5d0f93803d4d30be40a1509d02ebb1f340abc
Jeremy Hamilton <jeremy.hamilton@tekfive.com>
2022-04-30 19:18:56 -0700

    add data upload function

---
+++ load_data_to_postgres.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+DB_CONN_STRING = "postgres://postgres:postgres@127.0.0.1:5432/ctran"
+
+engine = create_engine(DB_CONN_STRING)
+
+def load_data(df: pd.DataFrame, table_name: str):
+  df.to_sql(table_name, engine)
