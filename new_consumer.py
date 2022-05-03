db4bf0acb8d999d5d5fe0ce52f66970c0ff576a6
Jeremy Hamilton <jeremy.hamilton@tekfive.com>
2022-05-03 07:17:02 -0700

    update code to more closely align with schema requirements

--- new_consumer.py
+++ new_consumer.py
@@ -1,141 +1,120 @@
 from confluent_kafka import Consumer
-from datetime import date, timedelta
+from datetime import date
 import pandas as pd
+import numpy as np
 import json
 import sys
 import ccloud_lib
 
 from load_data_to_postgres import load_data
 
-# Reformat the date 
-def formatDate(date):
-  datemap = {
-      'JAN': '1',
-      'FEB': '2',
-      'MAR': '3',
-      'APR': '4',
-      'MAY': '5',
-      'JUN': '6',
-      'JUL': '7',
-      'AUG': '8',
-      'SEP': '9',
-      'OCT': '10',
-      'NOV': '11',
-      'DEC': '12',
-  }
 
-  d = date.split('-')
-  ts = datemap[d[1]] + '-' + d[0] + '-' + d[2] + ' '
-
-  return ts
-
-def validate(raw):
+def validate(df: pd.DataFrame):
   error_log = open('error_log_'+str(date.today()),'w')
   # Assertion 1 (existence): All records have a lat/lon
-  if (raw['GPS_LATITUDE'] == '').any() or (raw['GPS_LONGITUDE'] == '').any() :
+  if (df['GPS_LATITUDE'] == '').any() or (df['GPS_LONGITUDE'] == '').any() :
     error_log.write('Assertion 1 failed: some records are missing lat/lon values')
-    raw = raw[raw['GPS_LATITUDE'] != np.nan]
-    raw = raw[raw['GPS_LONGITUDE'] != np.nan]
+    df = df[df['GPS_LATITUDE'] != np.nan]
+    df = df[df['GPS_LONGITUDE'] != np.nan]
   
   # Assertion 2 (inter-record): If bus has speed, it has direction
-  if not (raw['DIRECTION'].all() == raw['VELOCITY'].all()):
+  if not (df['DIRECTION'].all() == df['VELOCITY'].all()):
     error_log.write('Assertion 2 failed: some records have speed but not direction/direction but not speed')
-    raw = raw[(raw['VELOCITY'] == np.nan) == (raw['DIRECTION'] == np.nan)]
+    df = df[(df['VELOCITY'] == np.nan) == (df['DIRECTION'] == np.nan)]
 
   # Assertion 3 (limit): bus velocity must be beneath 80mph
-  if not (raw['VELOCITY']*2.236936 < 80).all():
+  if not (df['VELOCITY'] < 80).all():
     error_log.write('Assertion 3 failed: velocity exceeded 80mph')
-    raw = raw[raw['VELOCITY']*2.236936 < 80]
+    df = df[df['VELOCITY'] < 80]
 
   # Assertion 4 (summary): Meter count should always be ascending for a trip
-  trips = raw.groupby('EVENT_NO_TRIP')
-  test = pd.DataFrame(columns=['Trips'])
+  trips = df.groupby('EVENT_NO_TRIP')
   for trip, group in trips:
-    if (not pd.to_numeric(group['METERS']).is_monotonic_increasing):
-      error_log.write('Assertion 4 failed: meters werent ascending')
-      raw = raw[raw['EVENT_NO_TRIP' != trip]]
+    if (not group['METERS'].is_monotonic_increasing):
+      error_log.write("Assertion 4 failed: meters weren't ascending")
+      df = df[df['EVENT_NO_TRIP' != trip]]
                 
   # Assertion 5 (limit): Bus direction should be between 0 and 359
-  if (raw['DIRECTION'] < 0).any() or (raw['DIRECTION'] > 359).any():
+  if (df['DIRECTION'] < 0).any() or (df['DIRECTION'] > 359).any():
     error_log.write('Assertion 5 failed: bus direction was out of bounds')
-    raw = raw[raw['DIRECTION'] > 0]
-    raw = raw[raw['DIRECTION'] < 360]
+    df = df[df['DIRECTION'] > 0]
+    df = df[df['DIRECTION'] < 360]
     
-  # TODO Assertion doesn't work. must be cast to numeric before summing.
   # Assertion 6 (existence): No records should have repetition
-  if (raw.duplicated().any()):
+  if (df.duplicated().any()):
     error_log.write('Assertion 6 failed: some records are repeated')
     #if any records are repeated drop the duplicate values keeping the top
-    raw = raw.drop_duplicates()
+    df = df.drop_duplicates()
     
   # Assertion 7 (statistical): Satellite count for each trip is greater than 8 in average
-  stat = raw.groupby('EVENT_NO_TRIP').sum()/raw.groupby('EVENT_NO_TRIP').count()
-  if not (pd.to_numeric(stat['GPS_SATELLITES'] > 8).all()):
+  stat = df.groupby('EVENT_NO_TRIP').sum()/df.groupby('EVENT_NO_TRIP').count()
+  if not (stat['GPS_SATELLITES'] > 8).all():
     error_log.write('Assertion 7 failed: satellite count for each trip is less than 8 in average')
   
   # Assertion 8 (summary): The number of meters a vehicle travel must not exceed some reasonable limit (300,000?)
-  min_meter_value = raw.groupby('EVENT_NO_TRIP').min()
-  max_meter_value = raw.groupby('EVENT_NO_TRIP').max()
+  min_meter_value = df.groupby('EVENT_NO_TRIP').min()
+  max_meter_value = df.groupby('EVENT_NO_TRIP').max()
   diff_value = max_meter_value['METERS'] - min_meter_value['METERS']
   if (diff_value.max() > 300000):
     error_log.write('Assertion 8 failed: some vehicles exceeds the expected value of 300000 meters of travel')
 
   # Assertion 9 (intra-record): Single trip index should have single vehicle ID 
-  trip_vehichle = raw.groupby('EVENT_NO_TRIP').VEHICLE_ID.unique()
-  trip_vehicle_count = trip_vehichle.groupby(['EVENT_NO_TRIP']).count()
+  trip_vehicle = df.groupby('EVENT_NO_TRIP').VEHICLE_ID.unique()
+  trip_vehicle_count = trip_vehicle.groupby(['EVENT_NO_TRIP']).count()
   if (trip_vehicle_count.max() > 1):
     error_log.write('Assertion 9 failed: for a single trip more than one associated vehicle_id found')
     
   # Assertion 10 (limit): ACT_TIME should be > 0
-  if not (raw['ACT_TIME'].values.min() > 0):
+  if not (df['ACT_TIME'].values.min() > 0):
     error_log.write('Assertion 10 failed: clock time is invalid')
-    raw = raw[raw['ACT_TIME'] > 0]
+    df = df[df['ACT_TIME'] > 0]
     
   error_log.close()
 
-  return raw
+  return df
 
-#TODO make into its own function
-def fix_types(json_data):
-  json_data['ACT_TIME'] = pd.to_numeric(json_data['ACT_TIME'])
-  json_data['GPS_LATITUDE'] = pd.to_numeric(json_data['GPS_LATITUDE'])
-  json_data['GPS_LONGITUDE'] = pd.to_numeric(json_data['GPS_LONGITUDE'])
-  json_data['DIRECTION'] = pd.to_numeric(json_data['DIRECTION'])
-  json_data['EVENT_NO_TRIP'] = pd.to_numeric(json_data['EVENT_NO_TRIP'])
-  json_data['EVENT_NO_STOP'] = pd.to_numeric(json_data['EVENT_NO_STOP'])
-  json_data['VEHICLE_ID'] = pd.to_numeric(json_data['VEHICLE_ID'])
-  json_data['VELOCITY'].replace(to_replace='', value='0')
-  json_data['VELOCITY'] = pd.to_numeric(json_data['VELOCITY']).apply(lambda x: x*2.236936)
-  json_data['ACT_TIME'] = json_data['ACT_TIME'].apply(lambda x: str(timedelta(seconds=x)))
-  json_data['OPD_DATE'] = json_data['OPD_DATE'].apply(lambda x: formatDate(x))
+def fix_types(df: pd.DataFrame):
+  df['GPS_LATITUDE'] = pd.to_numeric(df['GPS_LATITUDE'])
+  df['GPS_LONGITUDE'] = pd.to_numeric(df['GPS_LONGITUDE'])
+  df['DIRECTION'] = pd.to_numeric(df['DIRECTION'])
+  df['EVENT_NO_TRIP'] = pd.to_numeric(df['EVENT_NO_TRIP'])
+  df['EVENT_NO_STOP'] = pd.to_numeric(df['EVENT_NO_STOP'])
+  df['VEHICLE_ID'] = pd.to_numeric(df['VEHICLE_ID'])
+  df['METERS'] = pd.to_numeric(df['METERS'])
+  df['VELOCITY'].replace(to_replace='', value='0')
+  df['VELOCITY'] = pd.to_numeric(df['VELOCITY']).apply(lambda x: x*2.236936)
+  df['ACT_TIME'] = pd.to_numeric(df['ACT_TIME'])
+  df['ACT_TIME'] = pd.to_timedelta(df['ACT_TIME'], unit='s')
+  df['OPD_DATE'] = pd.to_datetime(df['OPD_DATE'])
+  df['OPD_DATE'] = df['OPD_DATE'].dt.tz_localize(tz='US/Pacific')
   
-  return json_data
+  return df
 
-def reshape(json_data):
+def reshape(df: pd.DataFrame):
 
-  timestamp = json_data['OPD_DATE'] + json_data['ACT_TIME']
+  def isWeekday(day: str):
+    return day not in ["Sunday", "Saturday"]
+
+  timestamp = df['OPD_DATE'] + df['ACT_TIME']
+  weekday = timestamp.dt.day_name().apply(lambda x: "Weekday" if isWeekday(x) else x)
   # The first schema required for part B, "BreadCrumb"
-  BreadCrumb = json_data[[
-                                'GPS_LATITUDE', 
-                                'GPS_LONGITUDE', 
-                                'DIRECTION', 
-                                'VELOCITY',
-                                'EVENT_NO_TRIP'
-                              ]]
+  BreadCrumb = df[[
+    'GPS_LATITUDE', 
+    'GPS_LONGITUDE', 
+    'DIRECTION', 
+    'VELOCITY',
+    'EVENT_NO_TRIP'
+  ]]
   BreadCrumb = pd.concat([timestamp, BreadCrumb], axis=1)
   BreadCrumb.columns = ['tstamp','latitude','longitude','direction','speed','trip_id']
 
   # The second schema required for part B, "Trip". Note that columns 1, 4, and 5 are incomplete,
   # as mentioned in the assignment description
-  # TODO Service Key: one of ‘Weekday’, ‘Saturday’, or ‘Sunday’
-  # TODO Direction: needs to be 0 or 1 in the Trip table. Default to all 0's since not enough info.
   Trip = pd.concat([
-                          pd.concat([json_data[['EVENT_NO_TRIP','EVENT_NO_STOP','VEHICLE_ID']], timestamp,], axis=1),
-                          json_data['DIRECTION']
-                        ], axis=1)
+      pd.concat([df[['EVENT_NO_TRIP','EVENT_NO_STOP','VEHICLE_ID']], weekday,], axis=1),
+      df['DIRECTION']*0
+    ], axis=1)
   Trip.columns = ['trip_id','route_id','vehicle_id','service_key','direction']
-  BreadCrumb.to_json('Breadcrumb.json')
-  Trip.to_json('Trip.json')
 
   return BreadCrumb, Trip
 
@@ -159,7 +138,7 @@ if __name__ == '__main__':
 
     # Process messages
     total_count = 0
-    raw = pd.DataFrame(columns=[
+    df = pd.DataFrame(columns=[
         'EVENT_NO_TRIP',
         'EVENT_NO_STOP',
         'OPD_DATE',
@@ -186,7 +165,7 @@ if __name__ == '__main__':
             else:
                 # Check for Kafka message, add to dataframe
                 record_value = json.loads(msg.value())
-                raw.loc[total_count] = [
+                df.loc[total_count] = [
                   record_value['EVENT_NO_TRIP'],
                   record_value['EVENT_NO_STOP'],
                   record_value['OPD_DATE'],
@@ -207,13 +186,11 @@ if __name__ == '__main__':
         pass
     finally:
         # Leave group and validate+reshape data
-        raw = fix_types(raw)
-        raw = validate(raw)
-        BreadCrumb, Trip = reshape(raw)
+        df = fix_types(df)
+        df = validate(df)
+        BreadCrumb, Trip = reshape(df)
         print(BreadCrumb)
         print(Trip)
-        # TODO the date column is not being correctly translated.
-        # needs to be timestamp, but is currently 'text'.
         load_data(BreadCrumb, "BreadCrumb")
         load_data(Trip, "Trip")
         consumer.close()
