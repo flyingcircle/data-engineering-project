eefc4823b2966eb09114a78273d4f0abf4c0b8bf
Jeremy Hamilton <jeremy.hamilton@tekfive.com>
2022-05-08 13:04:54 -0700

    up the logging in consumer

--- new_consumer.py
+++ new_consumer.py
@@ -1,77 +1,75 @@
 #! /home/production/bus-py-data/bin/python
 from confluent_kafka import Consumer
-from datetime import date, timedelta
+from datetime import timedelta
+import logging
 import pandas as pd
 import numpy as np
 import json
-import sys
 import ccloud_lib
 
 from load_data_to_postgres import load_data
 
+logging.basicConfig(filename='logs/consumer.log', encoding='utf-8', level=logging.DEBUG)
 
 def validate(df: pd.DataFrame):
-  error_log = open('error_log_'+str(date.today()),'w')
   # Assertion 1 (existence): All records have a lat/lon
   if (df['GPS_LATITUDE'] == '').any() or (df['GPS_LONGITUDE'] == '').any() :
-    error_log.write('Assertion 1 failed: some records are missing lat/lon values')
+    logging.error('Assertion 1 failed: some records are missing lat/lon values')
     df = df[df['GPS_LATITUDE'] != np.nan]
     df = df[df['GPS_LONGITUDE'] != np.nan]
   
   # Assertion 2 (inter-record): If bus has speed, it has direction
   if not (df['DIRECTION'].all() == df['VELOCITY'].all()):
-    error_log.write('Assertion 2 failed: some records have speed but not direction/direction but not speed')
+    logging.error('Assertion 2 failed: some records have speed but not direction/direction but not speed')
     df = df[(df['VELOCITY'] == np.nan) == (df['DIRECTION'] == np.nan)]
 
   # Assertion 3 (limit): bus velocity must be beneath 80mph
   if not (df['VELOCITY'] < 80).all():
-    error_log.write('Assertion 3 failed: velocity exceeded 80mph')
+    logging.error('Assertion 3 failed: velocity exceeded 80mph')
     df = df[df['VELOCITY'] < 80]
 
   # Assertion 4 (summary): Meter count should always be ascending for a trip
   trips = df.groupby('EVENT_NO_TRIP')
   for trip, group in trips:
     if (not group['METERS'].is_monotonic_increasing):
-      error_log.write("Assertion 4 failed: meters weren't ascending")
+      logging.error("Assertion 4 failed: meters weren't ascending")
       df = df[df['EVENT_NO_TRIP' != trip]]
                 
   # Assertion 5 (limit): Bus direction should be between 0 and 359
   if (df['DIRECTION'] < 0).any() or (df['DIRECTION'] > 359).any():
-    error_log.write('Assertion 5 failed: bus direction was out of bounds')
+    logging.error('Assertion 5 failed: bus direction was out of bounds')
     df = df[df['DIRECTION'] > 0]
     df = df[df['DIRECTION'] < 360]
     
   # Assertion 6 (existence): No records should have repetition
   if (df.duplicated().any()):
-    error_log.write('Assertion 6 failed: some records are repeated')
+    logging.error('Assertion 6 failed: some records are repeated')
     #if any records are repeated drop the duplicate values keeping the top
     df = df.drop_duplicates()
     
   # Assertion 7 (statistical): Satellite count for each trip is greater than 8 in average
   stat = df.groupby('EVENT_NO_TRIP').sum()/df.groupby('EVENT_NO_TRIP').count()
   if not (stat['GPS_SATELLITES'] > 8).all():
-    error_log.write('Assertion 7 failed: satellite count for each trip is less than 8 in average')
+    logging.error('Assertion 7 failed: satellite count for each trip is less than 8 in average')
   
   # Assertion 8 (summary): The number of meters a vehicle travel must not exceed some reasonable limit (300,000?)
   min_meter_value = df.groupby('EVENT_NO_TRIP').min()
   max_meter_value = df.groupby('EVENT_NO_TRIP').max()
   diff_value = max_meter_value['METERS'] - min_meter_value['METERS']
   if (diff_value.max() > 300000):
-    error_log.write('Assertion 8 failed: some vehicles exceeds the expected value of 300000 meters of travel')
+    logging.error('Assertion 8 failed: some vehicles exceeds the expected value of 300000 meters of travel')
 
   # Assertion 9 (intra-record): Single trip index should have single vehicle ID 
   trip_vehicle = df.groupby('EVENT_NO_TRIP').VEHICLE_ID.unique()
   trip_vehicle_count = trip_vehicle.groupby(['EVENT_NO_TRIP']).count()
   if (trip_vehicle_count.max() > 1):
-    error_log.write('Assertion 9 failed: for a single trip more than one associated vehicle_id found')
+    logging.error('Assertion 9 failed: for a single trip more than one associated vehicle_id found')
     
   # Assertion 10 (limit): ACT_TIME should be >= 0
   if not (df['ACT_TIME'].min() >= timedelta(0)):
-    error_log.write('Assertion 10 failed: clock time is invalid')
+    logging.error('Assertion 10 failed: clock time is invalid')
     df = df[df['ACT_TIME'] >= timedelta(0)]
     
-  error_log.close()
-
   return df
 
 def fix_types(df: pd.DataFrame):
@@ -164,16 +162,18 @@ if __name__ == '__main__':
             msg = consumer.poll(10)
             if msg is None and len(df.index) > 0:
                 # Leave group and validate+reshape data
+                logging.debug(f"processing #{len(df.index)} breadcrumbs...")
                 df = fix_types(df)
                 df = validate(df)
                 BreadCrumb, Trip = reshape(df)
                 load_data(BreadCrumb, "BreadCrumb")
                 load_data(Trip, "Trip")
+                logging.debug(f"loaded data!")
                 df = getNewDf()
             elif msg is None:
                 continue
             elif msg.error():
-                print('error: {}'.format(msg.error()), file=sys.stdout)
+                logging.error(msg.error())
             else:
                 # Check for Kafka message, add to dataframe
                 try:
@@ -198,6 +198,7 @@ if __name__ == '__main__':
                 except:
                     continue
     except KeyboardInterrupt:
-        pass
+        pass      
     finally:
+        logging.error("Exiting service function. Should not happen!")
         consumer.close()
