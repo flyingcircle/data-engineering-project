fdf6e3c63eb187cf7cae33b6e8cc0557f91e9efe
Briana Oursler <briana.oursler@gmail.com>
2022-05-17 22:24:51 -0700

    Call python3 in consumer_service.sh

--- stop_consumer_service.sh
+++ stop_consumer_service.sh
@@ -1,3 +1,3 @@
 # /home/production/data-engineering-project/stop_consumer_service.sh
 #
-source /home/production/bus-py-data/bin/activate && /home/production/data-engineering-project/stop_consumer.py -f /home/production/.confluent/librdkafka_stop.config -t stop_event
+source /home/production/bus-py-data/bin/activate && python3 /home/production/data-engineering-project/stop_consumer.py -f /home/production/.confluent/librdkafka_stop.config -t stop_event
